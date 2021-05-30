54d450572144176fd9cbf5155d11b17e542460e0
WenHowe <zengwenhao0710@gmail.com>
2021-05-30 17:52:19 +0800

    while循环练习

---
+++ day_03/while_循环执行.py
@@ -0,0 +1,15 @@
+# coding = utf-8
+# author: Wenhowe
+# contact: zengwenhao0710@gmail.com
+# datetime: 2021/5/30 16:02
+# software: PyCharm
+
+"""
+文件说明：
+
+"""
+i = 0
+while i < 5:
+    print("i = : %d" % i)
+    # i = i + 1
+    i += 1
