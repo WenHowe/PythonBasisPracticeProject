dcde6dae79c16ac28aca63ab57af3fc89305021f
wenhowe <zengwenhao0710@gmail.com>
2021-05-31 00:48:45 +0800

    练习while循环

--- day_03/while_循环执行.py
+++ day_03/while_循环执行.py
@@ -8,8 +8,23 @@
 文件说明：
 
 """
+# 定义 计数器 i
 i = 0
-while i < 5:
-    print("i = : %d" % i)
-    # i = i + 1
+# while i < 5:
+#     print("i = : %d" % i)
+#     # i = i + 1
+#     i += 1
+
+# 计算0 - 100之间所有整数的和
+# 定义最终结果的变量
+result = 0
+# 循环体
+while i <= 100:
+    print("i = ： %d" % i)
+
+    # 循环一次，result需要和计数器想加一次
+    result += i
+
+    # 处理计数器
     i += 1
+print("result = ： %d" % result)
